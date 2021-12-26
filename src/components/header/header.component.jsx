5be7d8d88490851213c1efa69154390c82aa74c8
Gurmander <gmaan0008@gmail.com>
2021-12-26 13:12:43 -0800

    Cart Icon added with dropdown and cart reducer stuff

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg' 
 import {auth} from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -19,15 +21,20 @@ const Header = ({currentUser}) => (
         :
         <Link className='option' to='/signin'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
+    {
+      hidden ? null : <CartDropdown /> 
+    }
   </div>
 );
 
 //mapStateToProps when passed to connect, then
 // state there is our root-reducer which contains user: user-reducer that has currentUser value. So, instead of pulling the currentUser from app.js, we are grabbing it from root-reducer
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 });
 
 // connect- higher order fn that provides the access to redux related stuff. It takes in 2 arguments
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header); 
