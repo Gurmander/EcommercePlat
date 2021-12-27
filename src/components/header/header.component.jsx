d744e994278f3dc4b247f78807688b1719f2d02d
Gurmander <gmaan0008@gmail.com>
2021-12-26 23:20:35 -0800

    Checkout page add/remove CartItem done

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,9 @@ import {auth} from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { createStructuredSelector} from 'reselect';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selector';
 
 const Header = ({currentUser, hidden}) => (
   <div className="header">
@@ -31,9 +34,15 @@ const Header = ({currentUser, hidden}) => (
 
 //mapStateToProps when passed to connect, then
 // state there is our root-reducer which contains user: user-reducer that has currentUser value. So, instead of pulling the currentUser from app.js, we are grabbing it from root-reducer
-const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
-  currentUser,
-  hidden
+// const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+//   currentUser,
+//   hidden
+// });
+
+// createStructerdSelector- It passes the overall state to the selectors under the hood, instead of passing it manually as shown above.
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 // connect- higher order fn that provides the access to redux related stuff. It takes in 2 arguments
